c2bfbf83e95c7afe46aa0cb9ba1dbdb13b8b70d1
KiranpalKaurSandhu <133913687+KiranpalKaurSandhu@users.noreply.github.com>
2023-07-11 14:13:29 -0600

    first   temp  GUI
    first temporary gui

---
+++ Term4ProjectWorkshop/Term4ProjectWorkshop/frmMainForm.cs
@@ -0,0 +1,28 @@
+namespace Term4ProjectWorkshop
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlOnButtonPackages_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void btnPackages_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}

3b0372016681571be7a1eec639fa34fb4bee9b6b
Grayson Germsheid <grayson.germsheid@edu.sait.ca>
2023-07-18 14:16:55 -0600

    Suppliers Form

--- TravelExpertsAgencyGUI/TravelExpertsAgencyGUI/frmSuppliers.cs
+++ TravelExpertsAgencyGUI/TravelExpertsAgencyGUI/frmSuppliers.cs
@@ -18,55 +18,18 @@ namespace TravelExpertsAgencyGUI
             InitializeComponent();
         }
 
-        // Event handler for the "Exit" button click
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        // Event handler for the "frmSuppliers" form load event
         private void frmSuppliers_Load(object sender, EventArgs e)
         {
-            using (TravelExpertsContext db = new TravelExpertsContext())
-            {
-                // Retrieve and display data for suppliers in the DataGridView
-                dgvSuppliers.DataSource = db.Suppliers
-                    .Select(s => new
-                    {
-                        s.SupplierId,
-                        s.SupName
-                    }).ToList();
 
-                // Apply styling to the DataGridView
-                dgvSuppliers.EnableHeadersVisualStyles = false;
-                dgvSuppliers.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(51, 73, 107);
-                dgvSuppliers.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
-                dgvSuppliers.RowHeadersDefaultCellStyle.BackColor = Color.FromArgb(51, 73, 107);
-                dgvSuppliers.RowHeadersDefaultCellStyle.ForeColor = Color.White;
-                dgvSuppliers.DefaultCellStyle.BackColor = Color.White;
-                dgvSuppliers.DefaultCellStyle.ForeColor = Color.Black;
-                dgvSuppliers.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(225, 225, 225);
-                dgvSuppliers.Font = new Font("Segoe UI", 9);
-
-                // Change header text
-                dgvSuppliers.Columns[0].HeaderText = "Supplier ID";
-                dgvSuppliers.Columns[1].HeaderText = "Supplier Name";
-
-                // Set equal width for both columns
-                dgvSuppliers.Columns[0].FillWeight = 50;
-                dgvSuppliers.Columns[1].FillWeight = 50;
-
-                // Set column AutoSizeMode to Fill
-                dgvSuppliers.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-                dgvSuppliers.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-
-                // Auto resize columns
-                dgvSuppliers.AutoResizeColumns();
-            }
+            RefreshDisplay();
 
         }
 
-        // Method to open the "frmAddModifySuppliers" form inside the panel "pnlMainContent"
         private void openFormInPanel(Form mainForm, Form subForm)
         {
             // Find the panel control on the main form by its name
@@ -96,15 +59,140 @@ namespace TravelExpertsAgencyGUI
             }
         }
 
-        // Event handler for the "Add Suppliers" button click
-        private void btnAddSuppliers_Click(object sender, EventArgs e)
+        private void cmbSuppliers_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // Create the frmAddModifyPackages form and set its ParentForm property to this instance
-            frmAddModifySuppliers addForm = new frmAddModifySuppliers();
+            RefreshProducts();
+        }
+
+        private void RefreshDisplay()
+        {
+            int? selectedSupplier = cmbSuppliers.SelectedValue as int?;
+            int? selectedProduct = lstProducts.SelectedValue as int?;
+
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                cmbSuppliers.DataSource = db
+                    .Suppliers
+                    .Select(s => new { s.SupplierId, s.SupName })
+                    .ToList();
+
+                cmbSuppliers.ValueMember = "SupplierId";
+                cmbSuppliers.DisplayMember = "SupName";
+
+                if (selectedSupplier != null) 
+                    cmbSuppliers.SelectedValue = selectedSupplier;
+
+                RefreshProducts();
+            }
+        }
+
+        private void RefreshProducts()
+        {
+            int? selectedSupplier = cmbSuppliers.SelectedValue as int?;
+
+            if (selectedSupplier == null) return;
+
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                lstProducts.DataSource = db
+                .Suppliers
+                .Where(s => s.SupplierId == selectedSupplier)
+                .Join(
+                    db.ProductsSuppliers,
+                    s => s.SupplierId,
+                    ps => ps.SupplierId,
+                    (s, ps) => ps.ProductId)
+                .Join(
+                    db.Products,
+                    ps => ps,
+                    p => p.ProductId,
+                    (ps, p) => new { p.ProductId, p.ProdName })
+                .ToList();
+
+                lstProducts.ValueMember = "ProductId";
+                lstProducts.DisplayMember = "ProdName";
+            }
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemoveProduct_Click(object sender, EventArgs e)
+        {
+            int? selectedSupplier = cmbSuppliers.SelectedValue as int?;
+            int? selectedProduct = lstProducts.SelectedValue as int?;
+
+            if (selectedProduct == null || selectedSupplier == null)
+            {
+                return;
+            }
+
+            using (var db = new TravelExpertsContext())
+            {
+
+                var dbProdSup = db
+                    .ProductsSuppliers
+                    .Where(ps => ps.SupplierId == selectedSupplier
+                              && ps.ProductId == selectedProduct)
+                    .SingleOrDefault();
+
+                if (dbProdSup == null)
+                {
+                    MessageBox.Show($"Could not find product supplier for prod = {selectedProduct} and sup = {selectedSupplier}");
+                    return;
+                }
+
+                db.ProductsSuppliers.Remove(dbProdSup);
+                db.SaveChanges();
+
+                RefreshProducts();
+            }
+
+            //private void btnAddSuppliers_Click(object sender, EventArgs e)
+            //{
+            //    // Create the frmAddModifyPackages form and set its ParentForm property to this instance
+            //    frmAddModifySuppliers addForm = new frmAddModifySuppliers();
 
 
-            // Open the frmAddModifyPackages form in the same panel (pnlMainContent)
-            openFormInPanel(this.ParentForm, addForm);
+            //    // Open the frmAddModifyPackages form in the same panel (pnlMainContent)
+            //    openFormInPanel(this.ParentForm, addForm);
+            //}
+        }
+
+        private void btnAddSupplier_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemoveSupplier_Click(object sender, EventArgs e)
+        {
+            int? selectedSupplier = cmbSuppliers.SelectedValue as int?;
+
+            if (selectedSupplier == null)
+            {
+                return;
+            }
+
+            using (var db = new TravelExpertsContext())
+            {
+
+                var dbSupplier = db
+                    .Suppliers
+                    .Where(s => s.SupplierId == selectedSupplier)
+                    .SingleOrDefault();
+
+                if (dbSupplier == null)
+                {
+                    return;
+                }
+
+                db.Suppliers.Remove(dbSupplier);
+                db.SaveChanges();
+
+                RefreshDisplay();
+            }
         }
     }
 }
